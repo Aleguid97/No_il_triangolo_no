8ef8a95c4e527ae8fd14113a55e15eb93411b85a
Aleguid97 <aleguid97@gmail.com>
2024-01-30 14:26:35 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Triangolo
+{
+    internal class Triangolo_op
+    {
+        public int latoA { get; set; }
+        public int latoB { get; set; }
+        public int latoC { get; set; }
+
+        public string CheckTypeTriangle()
+        {
+            if (latoA == latoB && latoB == latoC)
+            {    
+                return "Il tuo rettagnolo è equilatero";
+            } 
+
+            else if (latoA == latoB ||  latoB == latoC || latoC == latoA)
+            {
+                return "il tuo rettangolo è isoscele";
+            }
+
+            else
+            {
+                return "Scaleno";
+            }
+        }
+
+        public Triangolo_op(int latoA, int latoB, int latoC)
+        {
+            this.latoA = latoA;
+            this.latoB = latoB;
+            this.latoC = latoC;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+            
+        {
+            Console.WriteLine("quanto è lungo il latoA?");
+            int latoA = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("quanto è lungo il latoB?");
+            int latoB = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("qauanto è lungo il latoC?");
+            int latoC = Convert.ToInt16(Console.ReadLine());
+
+            Triangolo_op triangolo = new Triangolo_op(latoA, latoB, latoC);
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine(triangolo.CheckTypeTriangle());
+            Console.ReadLine();
+        }
+    }
+}
